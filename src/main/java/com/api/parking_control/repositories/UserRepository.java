5a5b92ec07b0c67115e298929e2fd145bea6008c
Robnilson <84360474+Robnilson@users.noreply.github.com>
2024-10-22 22:40:32 -0300

    Adicionando autenticação

---
+++ src/main/java/com/api/parking_control/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.api.parking_control.repositories;
+
+import com.api.parking_control.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
